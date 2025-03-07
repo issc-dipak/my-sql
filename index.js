0993e71b8684066e9b8af94d3a6c9c6dd53f1d68
Dipak Patil <dipakpatil8589@gmail.com>
2025-03-06 23:16:38 -1200

    Fixed MySQL connection issue

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "md181023",
-  database: "Home",
+  host: process.env.ocalhost,      
+  user: process.env.root,      
+  password: process.env.md181023,  
+  database: process.env.Home,    
+  port: process.env.DB_PORT || 3306,  
 });
 
 con.connect((err) => {
   if (err) {
-    console.error("MySQL connection error:", err);
-    throw err;
+    console.error("🚨 MySQL connection error:", err);
+    return;
   }
-  console.log("MySQL Connected..");
+  console.log("✅ MySQL Connected Successfully!");
 });
 
 module.exports = con;
